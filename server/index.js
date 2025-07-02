36447d75d95f8c692dd98b1776f6c7cff9c72d42
Nickson Ogm <nicksonogm@gmail.com>
2025-07-02 10:22:05 +0300

    connection to database made

--- server/index.js
+++ server/index.js
@@ -1,14 +1,32 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 require("dotenv").config();
 const port = process.env.PORT;
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ MySQL connection error:", err.message);
+    return;
+  }
+  console.log("✅ MySQL connected");
+});
+
+module.exports = connection;
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Basic route
 app.get("/", (req, res) => {
-  res.send("Hello, World!");
+  res.send("Water Boat backend running!");
 });
 
 // Start server

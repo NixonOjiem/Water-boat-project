d825a0c19697d7aaf44e6e1168dff913311164b1
arkstudiodevelopers <nicksonogm@gmail.com>
2025-07-05 18:44:23 +0300

    safety commit

--- server/config/db.js
+++ server/config/db.js
@@ -7,6 +7,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
+  port: 3307,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,

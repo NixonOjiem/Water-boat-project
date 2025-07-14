22e376fb508a37ca2435d79d4d73446198e4b032
arkstudiodevelopers <nicksonogm@gmail.com>
2025-07-14 19:03:17 +0300

    resolved merge

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  port: 3306,
+  port: 3307,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
